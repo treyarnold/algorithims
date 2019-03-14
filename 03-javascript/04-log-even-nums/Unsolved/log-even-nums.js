7c9946dffcd354d896b5ef2be7993b04e4ebdc14
treyarnold <arnold.trey80@gmail.com>
2019-03-14 15:42:09 -0500

    log even nums done

---
+++ 03-javascript/04-log-even-nums/Unsolved/log-even-nums.js
@@ -0,0 +1,8 @@
+// Write code to print all numbers from 0 to `num`
+// Assume `num` will be a positive number
+
+var logEvenNums = function(num) {
+    for (let i=0; i <= num; i+= 2) {
+        console.log(i);
+    }
+};
--- 03-javascript/04-log-even-nums/Unsolved/log-nums.js
+++
@@ -1,4 +0,0 @@
-// Write code to print all numbers from 0 to `num`
-// Assume `num` will be a positive number
-
-var logEvenNums = function(num) {};
