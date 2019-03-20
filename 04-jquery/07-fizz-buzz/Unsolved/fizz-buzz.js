c8be25f0c29ad01c2106b3c62a21fdc51da1bc25
treyarnold <arnold.trey80@gmail.com>
2019-03-19 20:41:00 -0500

    fizz buzz done

--- 04-jquery/07-fizz-buzz/Unsolved/fizz-buzz.js
+++ 04-jquery/07-fizz-buzz/Unsolved/fizz-buzz.js
@@ -4,4 +4,17 @@
 // If a number is evenly divisible by both 3 AND 5, print "Fizz Buzz"
 // If a number is not divisible by 3 or 5, print the number
 
-var fizzBuzz = function(arr) {};
+var fizzBuzz = function(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let message = "";
+        if (arr[i] % 3 === 0) message += "Fizz"
+        if (arr[i] % 5 === 0) {
+            if (message === "") {
+                message += "Buzz";
+            } else {
+                message += " Buzz";
+            }}
+        if (message === "") console.log(arr[i]);
+        else console.log(message);
+    }
+};
