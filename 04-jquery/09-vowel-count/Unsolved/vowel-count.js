db7262df5d537e82c19bdc340f77d0bbac65a22e
treyarnold <arnold.trey80@gmail.com>
2019-03-19 20:49:34 -0500

    vowel count done

--- 04-jquery/09-vowel-count/Unsolved/vowel-count.js
+++ 04-jquery/09-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,11 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function(str) {
+    let count = 0;
+    let stringHolder = str.toLowerCase();
+    for (let i=0; i < str.length; i++){
+        if ((stringHolder[i] === "a") || (stringHolder[i] === "e") || (stringHolder[i] === "i") ||
+         (stringHolder[i] === "o") || (stringHolder[i] === "u")) count++
+    }
+    return count;
+};
