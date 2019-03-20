f4db5c6cf9f049b02b0fde1cfc1427ccbf046db6
treyarnold <arnold.trey80@gmail.com>
2019-03-19 21:25:23 -0500

    palindrome done

--- 05-timers/10-is-palindrome/Unsolved/is-palindrome.js
+++ 05-timers/10-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,9 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+    let median = Math.floor(str.length / 2);
+    for (i = 0; i < median; i++){
+        if (!(str[i] === str[str.length - 1 - i])) return false
+    };
+    return true    
+};
