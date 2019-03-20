4c5a13f7550d65d8e63085b77780f60c1d70aab7
treyarnold <arnold.trey80@gmail.com>
2019-03-19 20:43:32 -0500

    maxNum done

--- 04-jquery/08-max-num/Unsolved/max-num.js
+++ 04-jquery/08-max-num/Unsolved/max-num.js
@@ -1,3 +1,9 @@
 // Write code to return the largest number in the given array
 
-var maxNum = function(arr) {};
+var maxNum = function(arr) {
+    let num = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] > num) num = arr[i];
+    }
+    return num;
+};
